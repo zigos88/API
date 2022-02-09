7291ae8c74865afc6bb7b59689f7619d51015e85
Alexey Kalyakin <paxclub@yandex.ru>
2022-02-09 16:52:13 +0300

    added .gitignore

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+}
